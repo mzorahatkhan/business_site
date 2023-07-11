a733597f3641c56ab5d5ba5d0594debf4ae076cf
mzorahatkhan <mzorahatqa@gmail.com>
2023-07-11 17:08:08 -0400

    some attempt to seed data

--- src/main/java/com/mz/bdleather/BdLeatherApplication.java
+++ src/main/java/com/mz/bdleather/BdLeatherApplication.java
@@ -1,13 +1,36 @@
 package com.mz.bdleather;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mz.bdleather.dao.CustomerRepository;
+import com.mz.bdleather.dao.ProductRepository;
+import com.mz.bdleather.dao.SupplyRepository;
+import com.mz.bdleather.entities.Customer;
+import com.mz.bdleather.entities.Product;
+import com.mz.bdleather.entities.Supplier;
 
 @SpringBootApplication
 public class BdLeatherApplication {
+	
+	@Autowired
+	CustomerRepository custRepo;
+	
+	@Autowired
+	ProductRepository prodRepo;
+	
+	@Autowired
+	SupplyRepository supplyRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdLeatherApplication.class, args);
 	}
+	
+	
 
 }
---
+++ src/main/resources/tdata.sql
@@ -0,0 +1,12 @@
+-- This is to insert data in Customer table
+INSERT INTO customer (CUST_ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES 
+(1, 'John', 'Doe', 'john@example.com'),
+(2, 'Jane', 'Smith', 'jane@example.com'),
+(3, 'Michael', 'Johnson', 'michael@example.com'),
+(4, 'Emily', 'Brown', 'emily@example.com'),
+(5, 'David', 'Wilson', 'david@example.com'),
+(6, 'Sarah', 'Miller', 'sarah@example.com'),
+(7, 'James', 'Davis', 'james@example.com'),
+(8, 'Olivia', 'Taylor', 'olivia@example.com'),
+(9, 'Daniel', 'Anderson', 'daniel@example.com'),
+(10, 'Sophia', 'Thomas', 'sophia@example.com');

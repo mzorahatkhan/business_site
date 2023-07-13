e4fe691a8982c00a4868ff16515c9d04aa30ec7a
mzorahatkhan <mzorahatqa@gmail.com>
2023-07-12 21:24:13 -0400

    added a pie chart to display product page completed

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,17 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-	new Chart(document.getElementById("myPieChart"), {
-		type: 'pie',
-		data: {
-			labels: ['Brown', 'Blue', 'Black'],
-			datasets: [{
-				label: 'my dataset',
-				data: [5, 5, 10],
-				backgroundColor: ['brown', 'blue', 'black']
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false
+
+var chartDataStr = decodeMyHtml(productChartData);
+var chartDataJson = JSON.parse(chartDataStr);
+
+var numericData = [];
+var labelData = [];
+var backColor=[];
+var lengthProdCount = chartDataJson.length;
+
+for (i = 0; i < lengthProdCount; i++) {
+	numericData[i] = chartDataJson[i].productCount;
+	labelData[i] = chartDataJson[i].label;
+	//looping background color text according with the label
+	backColor[i]=chartDataJson[i].label;
+
+}
+
+
+//For a pie chart 
+new Chart(document.getElementById("myPieChart"), {
+	type: 'pie',
+	data: {
+		labels: labelData,
+		datasets: [{
+			label: 'Product Count',
+			data: numericData,
+			backgroundColor: backColor
+		}]
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false,
+		plugins: {
+			title: {
+				display: true,
+				text: 'Product Number Wtih  Color',
+				font: {
+					size: 20,
+					weight: 'bold'
+				}
+			}
 		}
-	});
+	}
 });
+
+//to decode the innter html that comeds from the mode as json string 
+function decodeMyHtml(html) {
+	var txt = document.createElement("textarea");// creating and element and assign to a textarea
+	txt.innerHTML = html;
+	return txt.value;
+
+}
